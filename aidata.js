f7a068623d322be9a29b6393d8e2e6e2bf6ab4d4
UppedHealer8521 <73911746+UppedHealer8521@users.noreply.github.com>
2020-12-07 12:05:12 -0800

    Create aidata.js

---
+++ aidata.js
@@ -0,0 +1,15 @@
+{
+	"General Galeforce": "government",
+	"Charles Calvin": "government",
+	"John Pilksin": "government",
+	"Rupert Price": "government",
+	"Gordon Banks": "government",
+	"Amelia Estaban": "government",
+	"Reginald Copperbottom": "toppat",
+	"Right Hand Man": "toppat",
+	"Sven Svensson": "toppat",
+	"Henry Stickmin": "toppat",
+	"Ellie Rose": "toppat",
+	"Slice": "toppat",
+	"Drimti,Petrov": "government"
+}
